6f17c8b4f39ae4623d4488f7215fa3c8432baabd
lsons2469 <lsonshrestha@gmail.com>
2021-01-14 15:59:50 -0600

    Emplementation of multiplicaiton method

--- MasterFile.java
+++ MasterFile.java
@@ -1,9 +1,24 @@
 public class MasterFile {
     public static void main(String[] args) {
 
+        int a = 5;
+        int b = 6;
+        System.out.println(multiMethod(a,b));
+
     }
 
     public void firstMethod(){
         System.out.println("First method");
+    }
+
+    public void alphaMethod(){
+        System.out.println("Alpha Uploaded")
+    }
+
+    public void multiMethod(int num1, int num2){
+        int num1;
+        int num2;
+        int result = num1 * num2;
+        return result;
     }
 }
